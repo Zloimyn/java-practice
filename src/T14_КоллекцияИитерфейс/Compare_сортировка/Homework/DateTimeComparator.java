6b5c54366a515388f19a28e03eec348135e4ce3f
Sergey <der222345@mail.ru>
2026-01-12 13:47:36 +0500

    ДЗ

---
+++ src/T14_КоллекцияИитерфейс/Compare_сортировка/Homework/DateTimeComparator.java
@@ -0,0 +1,19 @@
+package T14_КоллекцияИитерфейс.Compare_сортировка.Homework;
+
+import java.util.Comparator;
+
+public class DateTimeComparator implements Comparator<DateTime>{
+    @Override
+    public int compare(DateTime dt1, DateTime dt2) {
+        if (dt1.day == dt2.day){
+            if (dt1.hours == dt2.hours){
+                if (dt1.minutes == dt2.minutes){
+                    if (dt1.seconds == dt2.seconds){
+                        return -1;
+                    }
+                }
+            }
+        }
+        return Integer.compare(dt1.day, dt2.day);
+    }
+}
