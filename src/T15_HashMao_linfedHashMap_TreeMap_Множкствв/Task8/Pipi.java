4502aa79ef6ba3262784fca46287808f2fe797e6
runtime-1 <runtimestudent2023@gmail.com>
2026-02-06 17:29:46 +0500

    Set

--- src/T15_HashMao_linfedHashMap_TreeMap_Множкствв/Task8/Pipi.java
+++ src/T15_HashMao_linfedHashMap_TreeMap_Множкствв/Task8/Pipi.java
@@ -7,33 +7,30 @@ public class Pipi {
 
     public static void main(String[] args) {
         // создадим 1 миллион пользователей
-        for (long i = 1; i <= 10_000_000L; i++) {
-            users.put(new User(i, "i"), i);
+        for (long i = 1; i <= 1000000L; i++) {
+            users.put(new User(i, "Имя" + i), i);
+//            System.out.println(new User(i, "Имя" + i));
         }
 
         final long startTime = System.nanoTime();
-        Long user = findUser(378_366L);
+        Long user = findUser(5L);
         final long endTime = System.nanoTime();
 
         System.out.println("Найден пользователь: " + user);
         System.out.println("Поиск занял " + (endTime - startTime) + " наносекунд.");
+
+//        User user1 = new User(1L, "1");
+//        User user2 = new User(1L, "1");
+//
+//        System.out.println(user1.equals(user2));
+
+
     }
 
     private static Long findUser(Long userId) {
-        User user = new User(userId,"123");
+        User user = new User(userId,"Имя" + userId);
+        System.out.println(user);
         return users.get(user);
     }
 
-    @Override
-    public boolean equals(Object o){
-        if (this == o) return true;
-        if (o == null) return false;
-        User user = (User) o;
-        return ((User) o).id == user.id;
-    }
-
-    @Override
-    public int hashCode(){
-        return users.hashCode();
-    }
 }
--- src/T15_HashMao_linfedHashMap_TreeMap_Множкствв/Task8/User.java
+++ src/T15_HashMao_linfedHashMap_TreeMap_Множкствв/Task8/User.java
@@ -1,5 +1,7 @@
 package T15_HashMao_linfedHashMap_TreeMap_Множкствв.Task8;
 
+import java.util.Objects;
+
 public class User {
     Long id;
     String name;
@@ -13,5 +15,17 @@ public class User {
         return "User{id=" + id + ", name='" + name + "'}";
     }
 
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode(){
+        return id.hashCode() + name.hashCode();
+    }
 
 }
