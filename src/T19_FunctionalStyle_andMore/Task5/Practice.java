c2bb4874641aec240ea65b24d9233c6893c1efa8
runtime-1 <runtimestudent2023@gmail.com>
2026-06-08 17:37:00 +0500

    Anonumus

---
+++ src/T19_FunctionalStyle_andMore/Task5/Practice.java
@@ -0,0 +1,15 @@
+package T19_FunctionalStyle_andMore.Task5;
+
+public class Practice {
+    public static void main(String[] args) {
+// сохраните лямбду в переменную
+// лямбда должна складывать два целых числа
+        Summator summator = (n1, n2) -> n1 + n2;
+        // допишите код, использующий лямбду
+        System.out.println("Сумма 23 и 65 равна " + summator.sum(23, 65));
+    }
+}
+    @FunctionalInterface //добавьте аннотацию
+    interface Summator {
+        int sum(int n1, int n2);
+}
