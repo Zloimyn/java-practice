da150541b3e50d547c1f5dd1bc16c2fb0d8d70c7
Sergey <der222345@mail.ru>
2026-06-20 17:48:51 +0500

    Homework done

--- src/T20_Unixtime/Homework2/Practice.java
+++ src/T20_Unixtime/Homework2/Practice.java
@@ -2,12 +2,18 @@ package T20_Unixtime.Homework2;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
 public class Practice {
     public static void main(String[] args) {
-        String input = "14 часов 09 минут. Месяц: 02, День: 14, Год: 1966."; printCorrectDateTime(input);
+        String input = "14 часов 09 минут. Месяц: 02, День: 14, Год: 1966.";
+        printCorrectDateTime(input);
     }
-    private static void printCorrectDateTime(String input) { DateTimeFormatter inputFormatter = ... // определите входной формат
-        LocalDateTime dateTime = ... // сконвертируйте исходную строку в LocalDateTime
-        DateTimeFormatter outputFormatter = ... // определите выходной формат
-        System.out.println(...); // выведите результат на экран }
+
+    private static void printCorrectDateTime(String input) {
+        DateTimeFormatter inputFormatter =  DateTimeFormatter.ofPattern("HH часов mm минут. Месяц: MM, День: dd, Год: yyyy."); // определите входной формат
+        LocalDateTime dateTime = LocalDateTime.parse(input, inputFormatter); // сконвертируйте исходную строку в LocalDateTime
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ISO_DATE_TIME; // определите выходной формат
+        System.out.println(dateTime.format(outputFormatter)); // выведите результат на экран
     }
+
+}
