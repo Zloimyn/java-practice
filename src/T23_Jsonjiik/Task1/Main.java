79520af63411906aaea1b93998c06b7aa5b1fd4b
runtime-1 <runtimestudent2023@gmail.com>
2026-07-13 17:30:33 +0500

    T23 delaem

---
+++ src/T23_Jsonjiik/Task1/Main.java
@@ -0,0 +1,8 @@
+package T23_Jsonjiik.Task1;
+
+public class Main {
+//    public static void main(String[] args) {
+//        {"name":"Красавчик", "age":"2", "colour":"Серо-буро-малиновый", "animal":"Собака", "hobbies":["Игра в мяч","Поесть"]}
+//
+//    }
+}
